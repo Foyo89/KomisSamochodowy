d9b1a8a76402910b236b73fc5bdf799fd5c0191f
maciek_tbt4 <maciek_tbt4@o2.pl>
2017-08-09 21:06:42 +0200

    Filtrowanie

--- src/main/java/com/mycompany/controllers/ChangeCarBean.java
+++ src/main/java/com/mycompany/controllers/ChangeCarBean.java
@@ -5,6 +5,7 @@
  */
 package com.mycompany.controllers;
 
+import com.mycompany.Dtos.CarDto;
 import com.mycompany.komissamochodowy.database.ConfigHibernate;
 import com.mycompany.komissamochodowy.model.Car;
 import javax.enterprise.context.RequestScoped;
@@ -30,8 +31,9 @@ public class ChangeCarBean {
     
     
     
-    public String setCar(Car car) {
+    public String setCar(CarDto carDto) {
         
+        Car car = carToCarDto(carDto);
         this.car = car;
         return "changecar.xhtml";
     }
@@ -45,7 +47,27 @@ public class ChangeCarBean {
         return "showcars.xhtml";
     }
     
+    public Car carToCarDto(CarDto carDto)
+    {
+        Car newCar = new Car();
+        
+        newCar.setBrand(carDto.getBrand());
+        newCar.setDescription(carDto.getDescription());
+        newCar.setFuelType(carDto.getFuelType());
+        newCar.setId(carDto.getId());
+        newCar.setMilage(carDto.getMilage());
+        newCar.setModel(carDto.getModel());
+        newCar.setOcNumber(carDto.getOcNumber());
+        newCar.setPower(carDto.getPower());
+        newCar.setProductionYear(carDto.getProductionYear());
+        newCar.setRegistrationNumber(carDto.getRegistrationNumber());       
+        newCar.setTestDrive(carDto.getTestDrive());       
+        newCar.setTransmission(carDto.getTransmission());       
+        newCar.setVin(carDto.getVin());       
+        
+        return newCar;
     
+    }   
     
     
 }
---
+++ src/main/java/com/mycompany/controllers/ShowCarBean.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.controllers;
+
+import com.mycompany.Dtos.CarDto;
+import com.mycompany.komissamochodowy.database.ConfigHibernate;
+import com.mycompany.komissamochodowy.model.Car;
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author RENT
+ */
+@ManagedBean(name = "showCarBean")
+@RequestScoped
+public class ShowCarBean {
+
+    private String filterType;
+    private String filterValue;
+    
+    private CarDto carDto = new CarDto();    
+    private CarDto filter = new CarDto();
+    
+    private List<CarDto> carsDto = new ArrayList<>();
+
+    public List<CarDto> getCarsDto() {
+        
+        List<CarDto> returnedList = new ArrayList<>();
+        if(filterType == null || filterValue == null)
+        {                    
+            returnedList= getList(null);
+        }
+        else
+        {
+            if(filterType.equals("brak"))
+            {                        
+                returnedList= getList(null);
+            }
+            else
+            {            
+                returnedList= getList("FROM Car c where c."+ filterType +" like " + filterValue);                  
+            }
+        }
+        return returnedList;
+    }
+
+    public void setCarsDto(List<CarDto> carsDto) {
+        this.carsDto = carsDto;
+    }
+
+    
+    public String getFilterValue() {
+        return filterValue;
+    }
+
+    public void setFilterValue(String filterValue) {
+        this.filterValue = filterValue;
+    }    
+    
+    public String getFilterType() {
+        return filterType;
+    }
+
+    public void setFilterType(String filterType) {
+        this.filterType = filterType;
+    }
+
+    
+    public CarDto getCarDto() {
+        return carDto;
+    }
+
+    public void setCarDto(CarDto carDto) {
+        this.carDto = carDto;
+    }    
+
+    public CarDto getFilter() {
+        return filter;
+    }
+
+    public void setFilter(CarDto filter) {
+        this.filter = filter;
+    }   
+    
+    
+    
+    public List<CarDto> getList(String sQuery){
+        SessionFactory instance = ConfigHibernate.getInstance();
+        Session session = instance.openSession();
+       
+        Query query;
+        if(sQuery == null)
+        {              
+            query = session.createQuery("FROM Car");
+        }
+        else
+        {
+            query = session.createQuery(sQuery);             
+        }
+        
+        List <Car> cars = query.list();
+        
+        List<CarDto> carsDto = new ArrayList<>();
+        
+        for(Car c : cars)
+        {
+            CarDto newCarDto = new CarDto();
+            newCarDto = carToCarDto(c);
+            carsDto.add(newCarDto);        
+        }
+        
+        return carsDto;
+    }
+    
+    public void filteredTable()
+    {    
+//        if(filterType.equals("brak"))
+//        {                    
+//            getList(null);
+//        }
+//        else
+//        {
+//            getList("FROM Car c where c."+ filterType +" like " + filterValue);        
+//        }
+        
+        System.out.println("");
+    }
+    
+    
+   
+    public CarDto carToCarDto(Car car)
+    {
+        CarDto newCarDto = new CarDto();
+        
+        newCarDto.setBrand(car.getBrand());
+        newCarDto.setDescription(car.getDescription());
+        newCarDto.setFuelType(car.getFuelType());
+        newCarDto.setId(car.getId());
+        newCarDto.setMilage(car.getMilage());
+        newCarDto.setModel(car.getModel());
+        newCarDto.setOcNumber(car.getOcNumber());
+        newCarDto.setPower(car.getPower());
+        newCarDto.setProductionYear(car.getProductionYear());
+        newCarDto.setRegistrationNumber(car.getRegistrationNumber());       
+        newCarDto.setTestDrive(car.getTestDrive());       
+        newCarDto.setTransmission(car.getTransmission());       
+        newCarDto.setVin(car.getVin());       
+        
+        return newCarDto;
+    
+    }
+}
